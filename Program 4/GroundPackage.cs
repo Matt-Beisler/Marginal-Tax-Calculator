fff274d99ede5eaa5c47bce7fdf3d6463a66cfb9
Matt-Beisler <49101181+Matt-Beisler@users.noreply.github.com>
2020-12-01 21:12:59 -0500

    Add files via upload
    initial upload

---
+++ Program 4/GroundPackage.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+    public class GroundPackage
+    {
+        private int _originZip; //origin zip
+        private int _destinationZip; //destination zip
+        private double _length, _width, _height, _weight; //length, width, height, and weight
+        private int _zoneDistance; //distance between origin and destination zip
+        public const int zipLowThresh = 00000; //low threshold for zips
+        public const int zipHighThresh = 99999; //high threshold for zips
+        public const int dimensionLow = 0; //minimum for dimensions
+        public const double dimensionDefault = 1.0; //default value for dimensions if value is invalid
+        public const double SIZE_COST_FACTOR = .25; //size cost factor value
+        public const double WEIGHT_COST_FACTOR = .45; //weight cost factor value
+        public const int originDefault = 40202; //origin default if value is invalid
+        public const int destDefault = 90210; //destination default if value is invalid
+
+
+        public int OriginZip
+        {
+            //precondition: none
+            //postcondition: pacakges origin zip is returned
+            get
+            {
+                return _originZip;
+            }
+            //precondition: value >= 00000 && value <= 99999
+            //postcondition: packages origin zip is set to a value or the default if value is invalid
+            set
+            {
+                if (value > zipLowThresh && value <= zipHighThresh)
+                    _originZip = value;
+                else
+                    _originZip = originDefault; //when invalid, value = 40202
+            }
+        }
+
+        public int DestinationZip
+        {
+            //precondition: none
+            //postcondition: pacakges destination zip is returned
+            get
+            {
+                return _destinationZip;
+            }
+            //precondition: value >= 00000 && value <= 99999
+            //postcondition: packages destination zip is set to a value or the default if value is invalid
+            set
+            {
+                if (value > zipLowThresh && value <= zipHighThresh)
+                    _destinationZip = value;
+                else
+                    _destinationZip = destDefault; //when invalid, value is 90210
+            }
+        }
+        public double Length
+        {
+            //precondition: none
+            //postcondition: returns packages length
+            get
+            {
+                return _length;
+            }
+            //precondition: value > 0
+            //postcondtion: packages length is set to value or default if value is invalid
+            set
+            {
+                if (value > dimensionLow)
+                    _length = value;
+                else
+                    _length = dimensionDefault; //when invalid, value is 1.0
+            }
+        }
+        public double Width
+        {
+            //precondition: none
+            //postcondition: returns packages width
+            get
+            {
+                return _width;
+            }
+            //precondition: value > 0
+            //postcondtion: packages width is set to value or default if value is invalid
+            set
+            {
+                if (value > dimensionLow)
+                    _width = value;
+                else
+                    _width = dimensionDefault; //when invalid, value is 1.0
+            }
+        }
+        public double Height
+        {
+            //precondition: none
+            //postcondition: returns packages height
+            get
+            {
+                return _height;
+            }
+            //precondition: value > 0
+            //postcondtion: packages is set to value or default if value is invalid
+            set
+            {
+                if (value > dimensionLow)
+                    _height = value;
+                else
+                    _height = dimensionDefault; //when invalid, value is 1.0
+            }
+        }
+       public double Weight
+        {
+            //precondition: none
+            //postcondition: returns packages weight
+            get
+            {
+                return _weight;
+            }
+            //precondition: value > 0
+            //postcondtion: packages weight is set to value or default if value is invalid
+            set
+            {
+                if (value > dimensionLow)
+                    _weight = value;
+                else
+                    _weight = dimensionDefault; //when invalid, value is 1.0
+            }
+        }
+        public int ZoneDistance
+        {
+            //precondition: none
+            //postcondition: returns distance between zipcodes (read only)
+            get
+            {
+                _zoneDistance = Math.Abs((OriginZip / 10000) - (DestinationZip / 10000));
+                
+
+                return _zoneDistance;
+            }
+        }
+        //precondition: none
+        //postcondition: returns the calculated cost of shipping the package
+        public double CalcCost()
+        {
+            return SIZE_COST_FACTOR * (Length + Width + Height) + WEIGHT_COST_FACTOR * (ZoneDistance + 1) * (Weight);
+        }
+        //precondition: none
+        //postcondition: packages information is returned as a string
+        public override string ToString()
+        {
+            return $"Orgin Zipcode is {OriginZip}, destination zipcode is {DestinationZip}, zone distance is {ZoneDistance} {Environment.NewLine}" +
+                $"Package length is {Length}, width is {Width}, height is {Height}, weight is {Weight} {Environment.NewLine}" + $"Cost of shipping is {CalcCost():C}";
+                
+        }
+        //precondition: none
+        //postcondition: GroundPackage is created with all the needed values (constructor)
+        public GroundPackage(int oz = 00000, int dz = 00000, double l = 1.0, double w = 1.0, double h = 1.0, double wt = 1.0)
+        {
+            OriginZip = oz; //set using origin zip property
+            DestinationZip = dz; //set using destination zip property
+            Length = l; //set using the length property
+            Width = w; //set using the width property
+            Height = h; //set using the height property
+            Weight = wt; //set using the weight property
+
+        }
+        
+
+         
+    }
+}
---
+++ Program 4/Program.cs
@@ -0,0 +1,64 @@
+﻿//Grading ID:N4999
+//Program 4
+//Due 4/23/2019
+//CIS 199-75
+//Program displays all package information used for shipping including shipping costs, changes property data, and reprints the new package information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+    class Program
+    {
+        //precondition: none
+        //Postcondition: Groundpackages information is displayed, changed, and displayed again
+        public static void Main(string[] args)
+        {
+            GroundPackage package1 = new GroundPackage(40216, 90216, 5, 8, 1, 10.5); //values for the 5 initial packages
+            GroundPackage package2 = new GroundPackage(51846, 75036, 3.7, 7.6, 7.6, 5);
+            GroundPackage package3 = new GroundPackage(16749, 45824, 4, 7, 3, 15);
+            GroundPackage package4 = new GroundPackage(00054, 74125, 7.3, 5.8, 4, 13.8);
+            GroundPackage package5 = new GroundPackage(40218, 82541, 4, 8, 2.5, 25);
+
+            GroundPackage[] groundpackages = new GroundPackage[5]; //create array with 5 values
+
+            groundpackages[0] = package1; //initialize array with values above
+            groundpackages[1] = package2;
+            groundpackages[2] = package3;
+            groundpackages[3] = package4;
+            groundpackages[4] = package5;
+
+            foreach (GroundPackage package in groundpackages) //writes information for all packages
+            {
+                Console.WriteLine($"{package}"); //uses ToString
+                Console.WriteLine($"package costs {package.CalcCost():C}");
+                Console.WriteLine("");
+                Console.WriteLine($"After package changes {Environment.NewLine}");
+                
+
+                package.Height += 5; //values properties are changed
+                package.Weight += 3;
+                package.Length -= 4;
+                package.OriginZip += 437;
+                package.Width -= 2;
+
+
+                Console.WriteLine($"{package}"); //new information is displayed using changed properties
+                Console.WriteLine($"package now costs {package.CalcCost():C}");
+                Console.WriteLine("");
+            }
+
+
+
+
+
+
+        }
+
+
+       
+    }
+}
